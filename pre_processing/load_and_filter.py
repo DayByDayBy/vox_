21a57b055f5a79aad607b485144d10f3ad7acc8b
Diomedes Git <diomedesgit@gmail.com>
2025-06-20 17:18:29 +0100

    better load and filter, also added wee explanatory comment

--- pre_processing/load_and_filter.py
+++ pre_processing/load_and_filter.py
@@ -1,8 +1,20 @@
 import pandas as pd
 from pathlib import Path
-from typing import Union3aASsdfjh. 
+from typing import Union
+
+
+# self doc, really... but the basic idea is taking the TSV, grabbing the gender and
+# age range needed (lol at fourties), filtering those to remove non-native
+# speakers, and then to grab american voices - 20-59, us-eng, fem
 
 def load_and_filter_tsv(tsv_path: Union[str, Path]) -> pd.DataFrame:
+    
+    """
+    self doc, really... but the basic idea is taking the TSV, grabbing the gender 
+    and age range needed (lol at fourties), filtering those to remove non-native
+    speakers, and then to grab american voices - 20-59, us-eng, fem
+    """
+
     df = pd.read_csv(tsv_path, sep="\t")
 
     age_group = {"twenties", "thirties", "fourties", "fifties"}
@@ -10,6 +22,15 @@ def load_and_filter_tsv(tsv_path: Union[str, Path]) -> pd.DataFrame:
         (df["gender"] == "female") & 
         (df["age"].isin(age_group))
     ]
+
+    # accent column normalising (some empties, etc)
+    filtered["accent"] = filtered["accent"].fillna("").str.lower()
+
+    #  non-native speaker entry filtering
+    filtered = filtered[~filtered["accent"].str.contains("non native")]
+
+    # 'american' filter 
+    filtered = filtered[filtered["accent"].str.contains("american")]
 
     return filtered
 
